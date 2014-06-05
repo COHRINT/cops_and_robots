df13326e1fe0b1fbe4ed181dc38be49317db3a5e
Nick Sweet <nckswt@gmail.com>
2014-06-05 17:01:07 -0600

    Fixed byte conversion

--- cops_and_robots/Cop.py
+++ cops_and_robots/Cop.py
@@ -2,9 +2,8 @@ from cops_and_robots.Robot import Robot
 
 class Cop(Robot):
 	"""docstring for Cop"""
-	def __init__(self):
+	def __init__(self, name = "Deckard"):
 		#Superclass attributes
-		name = "Deckard"
 		Robot.__init__(self,name)
 		
 		
