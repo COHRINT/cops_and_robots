6d47e52631f5330501780de4e5204ec9b33c86da
Nick Sweet <nckswt@gmail.com>
2014-06-23 12:16:08 -0600

    removing improperly named files

--- src/Cop.py
+++
@@ -1,9 +0,0 @@
-from cops_and_robots.Robot import Robot
-
-class Cop(Robot):
-	"""docstring for Cop"""
-	def __init__(self, name = "Deckard"):
-		#Superclass attributes
-		Robot.__init__(self,name)
-		
-		
