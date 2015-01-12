ec94b582d3fd47300610bf896b736377c0533f5e
Nick Sweet <nckswt@gmail.com>
2015-01-12 01:33:24 -0700

    moving and scaling camera based on map objects

--- src/cops_and_robots/MapObj.py
+++ src/cops_and_robots/MapObj.py
@@ -14,11 +14,11 @@ class MapObj(object):
     """Generate a map object based on a geometric shape, plus 'zones'
 
         :param name: String
-        :param shape: #[(x_i,y_i)] in [m] as a list of positive xy pairs
-        :param pose: #[x,y,theta] in [m,m,deg] as the pose of the centroid
-        :param shape: #[(x_i,y_i)] in [m] as a list of positive xy pairs
+        :param shape: [(x_i,y_i)] in [m] as a list of positive xy pairs
+        :param pose: [x,y,theta] in [m,m,deg] as the pose of the centroid
+        :param shape: [(x_i,y_i)] in [m] as a list of positive xy pairs
         """
-    def __init__(self,name,shape,pose=[0,0,0],has_zones=True):
+    def __init__(self,name,shape_pts,pose=[0,0,0],has_zones=True,centroid_at_origin=True):
         self.name = name #sting identifier
         self.has_zones = has_zones
         
@@ -26,24 +26,34 @@ class MapObj(object):
         self.pose = pose #[x,y,theta] in [m] coordinates of the centroid in the global frame
         
         #If shape has only length and width, convert to point-based poly
-        if len(shape) == 2:
-            shape = [(0,0),(0,shape[1]),(shape[0],shape[1]),(shape[0],0),(0,0)]
+        if len(shape_pts) == 2:
+            shape_pts = [(0,0),(0,shape_pts[1]),(shape_pts[0],shape_pts[1]),(shape_pts[0],0),(0,0)]
 
         #Define shape as a list of points (assume only solid polygons for now)
         #Construct the shape such that the centroid angle (the direction the object normally faces) is 0
-        self.shape = Polygon(shape) #[(x_i,y_i)] in [m] as a list of positive xy pairs 
-        x,y = self.shape.centroid.x, self.shape.centroid.y
-        shape = [( p[0]-x, p[1]-y ) for p in shape] #place centroid at origin
-        self.shape = Polygon(shape)
+        if centroid_at_origin:
+            shape = Polygon(shape_pts) #[(x_i,y_i)] in [m] as a list of positive xy pairs 
+            x,y = shape.centroid.x, shape.centroid.y
+            shape_pts = [( p[0]-x, p[1]-y ) for p in shape_pts] #place centroid of shape at origin (instead of minimum point)
+        self.shape = Polygon(shape_pts)
 
         #place the shape at the correct pose
         self.move_shape(pose)
 
-    def move_shape(self,pose):
+    def move_shape(self,pose,rotation_pt="centroid"):
+        """Translate and rotate the shape
+
+        :param pose: [x,y,theta] in [m,m,deg] as the pose of the centroid
+        """
+        if rotation_pt is "centroid":
+            rotation_point = self.shape.centroid
+        else:
+            rotation_point = rotation_pt
+        self.rotate_poly(pose[2],rotation_point)
+
         self.pose = pose
-        self.shape = [( p[0]+pose[0], p[1]+pose[1] ) for p in self.shape.exterior.coords]
-        self.shape = Polygon(self.shape)
-        self.rotate_poly(pose[2],self.shape.centroid)
+        shape_pts = [( p[0]+pose[0], p[1]+pose[1] ) for p in self.shape.exterior.coords]
+        self.shape = Polygon(shape_pts)
 
         self.sides = []
         self.zones = []
@@ -54,7 +64,27 @@ class MapObj(object):
         if self.has_zones:
             self.define_zones()
 
+        return Polygon(shape_pts)
+
+    def rotate_poly(self,angle,rotation_point):
+        pts = self.shape.exterior.coords
+        lines = []
+        for pt in pts:
+            line = LineString([rotation_point,pt])
+            lines.append(rotate(line,angle,origin=rotation_point))
+
+        pts = []
+        for line in lines:
+            pts.append(line.coords[1])
+
+        self.shape = Polygon(pts)
+
     def define_zones(self,zone_distance=0.5):
+        """Define areas near the shape ('zones') which, for a four-sided shape, demarcate front, back left and right
+
+        :param zone_distance: Double in [m] from the outermost edge of the shape
+        """
+
         resolution = 10
         round_ = 1
         mitre = 2
@@ -85,19 +115,6 @@ class MapObj(object):
             self.zones_by_label['right'] = self.zones[2]
             self.zones_by_label['back'] = self.zones[3]
 
-    def rotate_poly(self,angle,rotation_point=(0,0)):
-        pts = self.shape.exterior.coords
-        lines = []
-        for pt in pts:
-            line = LineString([rotation_point,pt])
-            lines.append(rotate(line,angle,origin=rotation_point))
-
-        pts = []
-        for line in lines:
-            pts.append(line.coords[1])
-
-        self.shape = Polygon(pts)
-
     def add_to_plot(self,ax,color=BLUE,include_shape=True,include_zones=False):
         patch = PolygonPatch(self.shape, facecolor=color, alpha=0.5, zorder=2)
         ax.add_patch(patch)
@@ -113,45 +130,60 @@ class MapObj(object):
 if __name__ == '__main__':
     l = 1.2192 #[m] wall length
     w = 0.1524 #[m] wall width
-    shape = [(0,0),(0,w),(l,w),(l,0),(0,0)]
-    pose = (2.5,1.5,45)
-    wall1 = MapObj('wall1',shape,pose)
-
-    shape = [(0,0),(l/2,w*10),(l,0),(l/2,-w*10),(0,0)]
-    pose = (0,0,0)
-    wall2 = MapObj('wall2',shape,pose)
-
-    shape = [(0,0),(l/2,w*4),(l,0),(0,0)]
-    pose = (2,-2,30)
-    wall3 = MapObj('wall3',shape,pose)
-
-    p = Point(0,0)
-    circle = p.buffer(1)
-    shape = circle.exterior.coords
-    pose = (-2.5,-2,30)
-    wall4 = MapObj('wall4',shape,pose)
-
 
     fig = plt.figure(1,figsize=(10,6)) 
     ax = fig.add_subplot(111)
+
+    shape = [(0,0),(3*l,w),(3*l,-w),(0,0)]
+    pose = (2,-2,0)
+    wall1 = MapObj('wall3',shape,pose)
 
     wall1.add_to_plot(ax,include_zones=False) 
-    wall2.add_to_plot(ax,include_zones=False) 
-    wall3.add_to_plot(ax,include_zones=False) 
-    wall4.add_to_plot(ax,include_zones=False) 
+
+    wall1.move_shape((0,0,90),wall1.shape.exterior.coords[0])
+
+    wall1.add_to_plot(ax,color=RED,include_zones=False) 
+
+
+    # shape = [(0,0),(0,w),(l,w),(l,0),(0,0)]
+    # pose = (2.5,1.5,45)
+    # wall1 = MapObj('wall1',shape,pose)
+
+    # shape = [(0,0),(l/2,w*10),(l,0),(l/2,-w*10),(0,0)]
+    # pose = (0,0,0)
+    # wall2 = MapObj('wall2',shape,pose)
+
+    # shape = [(0,0),(l/2,w*4),(l,0),(0,0)]
+    # pose = (2,-2,30)
+    # wall3 = MapObj('wall3',shape,pose)
+
+    # p = Point(0,0)
+    # circle = p.buffer(1)
+    # shape = circle.exterior.coords
+    # pose = (-2.5,-2,30)
+    # wall4 = MapObj('wall4',shape,pose)
+
+
+    # fig = plt.figure(1,figsize=(10,6)) 
+    # ax = fig.add_subplot(111)
+
+    # wall1.add_to_plot(ax,include_zones=False) 
+    # wall2.add_to_plot(ax,include_zones=False) 
+    # wall3.add_to_plot(ax,include_zones=False) 
+    # wall4.add_to_plot(ax,include_zones=False) 
     
-    lim = 5
-    ax.set_xlim([-lim,lim])
-    ax.set_ylim([-lim,lim])
-    plt.show()      
+    # lim = 5
+    # ax.set_xlim([-lim,lim])
+    # ax.set_ylim([-lim,lim])
+    # plt.show()      
 
-    fig = plt.figure(1,figsize=(10,6)) 
-    ax = fig.add_subplot(111)
+    # fig = plt.figure(1,figsize=(10,6)) 
+    # ax = fig.add_subplot(111)
 
-    wall1.add_to_plot(ax,include_zones=True) 
-    wall2.add_to_plot(ax,include_zones=True) 
-    wall3.add_to_plot(ax,include_zones=True) 
-    wall4.add_to_plot(ax,include_zones=True) 
+    # wall1.add_to_plot(ax,include_zones=True) 
+    # wall2.add_to_plot(ax,include_zones=True) 
+    # wall3.add_to_plot(ax,include_zones=True) 
+    # wall4.add_to_plot(ax,include_zones=True) 
     
     lim = 5
     ax.set_xlim([-lim,lim])
