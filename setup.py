e671065530f2675eec304dc5f273dbff70c69f57
Nick Sweet <nckswt@gmail.com>
2015-06-08 14:28:02 -0600

    adding setuptools and travis-ci

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
+import ez_setup
+ez_setup.use_setuptools()
 
 setup_args = generate_distutils_setup(
+    
+    # Author information and Metadata
     name='cops_and_robots',
+    version=cops_and_robots.__version__,
+    license='Apache Software License',
     author='Nick Sweet',
     author_email='nick.sweet@colorado.edu',
-    packages=['cops_and_robots'],
-	package_dir={'':'src'},    
+
+    # Package data
+    packages=find_packages(),
+    package_dir={'cops_and_robots':'src/cops_and_robots'},
     include_package_data=True,
     platforms='any',
     requires=['std_msgs','rospy'],
     tests_require=['pytest'],
-    install_requires=['getch>=1.0'],
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     scripts=['scripts'],
 )
 
